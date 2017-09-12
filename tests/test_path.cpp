2248ac95a284fdbd193ae07cfb19c0f9c16c2ff9
Andreas Runfalk <andreas@runfalk.se>
2017-09-12 09:59:15 +0200

    Added test cases for Path::has_valid_extension

--- tests/test_path.cpp
+++ tests/test_path.cpp
@@ -1,4 +1,6 @@
 #include <doctest.h>
+#include <string>
+#include <vector>
 
 #include "path.h"
 
@@ -7,4 +9,17 @@ TEST_CASE("Test has_extension") {
 	CHECK(!Path::has_extension("foo.txt", ".bar"));
 	CHECK(Path::has_extension("foo", ""));
 }
- 
+
+TEST_CASE("Test has_valid_extension") {
+	std::vector<std::string> extensions;
+	extensions.push_back(".txt");
+	extensions.push_back(".h");
+	extensions.push_back(".cpp");
+
+	CHECK(Path::has_valid_extension("test.txt", extensions));
+	CHECK(Path::has_valid_extension("test.h", extensions));
+	CHECK(Path::has_valid_extension("test.cpp", extensions));
+	CHECK(!Path::has_valid_extension("test.cc", extensions));
+	CHECK(!Path::has_valid_extension("test.hpp", extensions));
+	CHECK(!Path::has_valid_extension("test.py", extensions));
+}
